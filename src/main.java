4cbac3894c647fb21d8afe48be0c84c3ad70366a
Pedro Pablo Sanchez Nualart <p.sanchez13@ufromail.cl>
2026-09-02 00:39:16 -0400

    Realize el merge final

--- src/main.java
+++ src/main.java
@@ -1,23 +1,25 @@
 public class main {
 
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Sebastian Meier");
 
 
     }
+
     public static void main(String[] args) {
     public static  void main(String[] args){
         System.out.println("Hola Mundo!!!!");
         System.out.println("Hola soy Diego Rifoooo !!!!");
-    public static void main(String[] args){
+    }
+
+    public static void main(String[] args) {
         System.out.println("hola mundo !!!!");
         System.out.println("soy Nureddin Muñoz");
         System.out.println("Pedro Sanchez");
     }
-}
 
 
-    }//diegoooo
+    //diegoooo
 }
