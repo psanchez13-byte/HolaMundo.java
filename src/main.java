b2d2d18d0cf8277091b4f82c532577f0f6238cbd
diegoo <d.rifo01@ufromail.cl>
2026-09-02 00:19:19 -0400

    Rama de diego correcion de vercion incompatible

--- src/main.java
+++ src/main.java
@@ -1,2 +1,8 @@
 public class main {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!!!!");
+        System.out.println("Hola soy Diego Rifoooo !!!!");
+
+
+    }//diegoooo
 }
